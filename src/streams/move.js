b43349e638d2734f9691c969dffc87efe9d68ceb
Alex <al.gr.0017@gmail.com>
2022-12-19 14:02:02 +0300

    feat: add solution

--- src/streams/move.js
+++ src/streams/move.js
@@ -4,14 +4,15 @@ import { resolve, basename } from "path";
 
 export const move = async (baseFile, destination) => {
   const readableStream = createReadStream(resolve(baseFile));
-  const writableStream = createWriteStream(resolve((destination), basename(baseFile)));
+  const writableStream = createWriteStream(
+    resolve(destination, basename(baseFile))
+  );
 
   try {
     readableStream.pipe(writableStream);
-    readableStream.on('close', function () {
-        rm(resolve(baseFile));
+    readableStream.on("close", function () {
+      rm(resolve(baseFile));
     });
-
   } catch (error) {
     console.error("Operation failed");
   }
